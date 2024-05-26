09ab5ae53760b091ec2dd4d33f6a7f7a11fdb8cc
charmful0x <charmful0x@gmail.com>
2024-05-27 00:46:50 +0200

    feat: init

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use axum::{
+    extract::{Json, State},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::post,
+    Router,
+};
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use std::env;
+use dotenv::dotenv;
+use std::str::FromStr;
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RpcRequest {
+    jsonrpc: String,
+    method: String,
+    params: Value,
+    id: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RpcResponse {
+    jsonrpc: String,
+    id: u64,
+    result: Option<Value>,
+    error: Option<Value>,
+}
+
+struct AppState {
+    client: Client,
+    eth_rpc_url: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+    let eth_rpc_url = "http://34.141.88.80:8545".to_string();
+
+    let shared_state = Arc::new(Mutex::new(AppState { client, eth_rpc_url }));
+
+    let app = Router::new()
+        .route("/rpc", post(handle_rpc_request))
+        .with_state(shared_state);
+    let port: u16 = std::env::var("PORT").unwrap_or(String::from("8888")).parse().expect("invalid port");
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    println!("Server running on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handle_rpc_request(
+    State(state): State<Arc<Mutex<AppState>>>,
+    Json(req): Json<RpcRequest>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let state = state.lock().await;
+
+    let response = state
+        .client
+        .post(&state.eth_rpc_url)
+        .json(&req)
+        .send()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .json::<RpcResponse>()
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok((StatusCode::OK, Json(response)))
+}
